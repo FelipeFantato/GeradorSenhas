a4566af2475856fe205c70b73bf9c552e0bba6cb
F0sfato <felipe.fantato3@gmail.com>
2022-02-13 20:35:17 -0300

    Create Gerador de senhas.py

---
+++ Gerador de senhas.py
@@ -0,0 +1,28 @@
+import random
+
+#site = input("digite o nome do site: ")
+#login = input("digite seu o nome de login: ")
+tamanho = input("qual seria o tamanho da senha?")
+
+def geradorSenha(tamanho):
+    
+    i = 0
+    novaSenha = ""
+    
+    while(int(tamanho) > i):
+        print(i)
+        listaMinuscula = 'abcdefghijklmnopqrstuvwxyz'
+        listaMaiuscula = listaMinuscula.upper()
+        listaNumeros = "0123456789"
+        listaCaractere = "!@#$%^&*'~`"
+        lista = listaMinuscula + listaMaiuscula + listaNumeros + listaCaractere
+        listaMisturada = ''.join(random.sample(lista,len(lista)))
+        #print(listaMisturada)
+        caractere = random.choices(listaMisturada)
+        novaSenha = novaSenha + ''.join(caractere)
+        
+        i = i+1   
+        
+    return novaSenha
+
+print(geradorSenha(tamanho))
